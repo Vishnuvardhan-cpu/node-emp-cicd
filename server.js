3b7286223e948e9d2b260847290c0f39250f7672
Vishnu <vishnuvvv259@gmail.com>
2026-05-27 05:38:17 +0000

    Updated

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send(`
         <div style="text-align:center; margin-top:50px; font-family:Arial;">
-            <h1 style="color:#0078d4;">Vishnu's Automated Enterprise! 🤖⚡</h1>
-            <p style="font-size:18px;">CI/CD Pipeline is successfully running using GitHub Actions!</p>
+            <h1 style="color:#0078d4;">Vishnuvardhan Automated Enterprise! 🤖⚡</h1>
+            <p style="font-size:18px;">CI/CD Pipeline is successfully running using GitHub Actions! happy happy</p>
             <div style="background:#e1dfdd; padding:20px; display:inline-block; border-radius:8px;">
                 <h3>Deployment Status: Auto-Updated 🟢</h3>
                 <p>Automation Lead: Vishnu Vardhan</p>
